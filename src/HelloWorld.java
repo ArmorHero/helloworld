2e594210a7ca2fd02afc40621eb79b8039ce1e42
lenovo <lcoffice2017@163.com>
2020-04-19 12:36:38 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by lenovo on 2020/4/19.
+ * Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
